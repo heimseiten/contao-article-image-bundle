11d488885b7b64c680087b453766208e9571a90c
Niels <info@heimseiten.de>
2021-04-18 22:05:03 +0200

    Add files via upload

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -1,6 +1,20 @@
 <?php
+
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
-use Contao\DataContainer;
+use Contao\System;
+
+PaletteManipulator::create()
+    ->addLegend('article_bg', 'title_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('articleImage', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('articleVideo', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('size', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('verticalBgShift', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('viewBgImageOnMobile', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('viewBgVideoOnMobile','article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('bgParallax', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('noBgVideoLoop', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->addField('BgCssFilter', 'article_bg', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_article');
 
 $GLOBALS['TL_DCA']['tl_article']['fields']['articleImage'] = [ 
     'label'     => &$GLOBALS['TL_LANG']['tl_article']['articleImage'],
@@ -8,61 +22,62 @@ $GLOBALS['TL_DCA']['tl_article']['fields']['articleImage'] = [
     'eval' => array('tl_class'  => 'w50', 'fieldType' => 'radio', 'filesOnly' => true, 'extensions' => \Config::get('validImageTypes') ),
     'sql'       => "blob NULL"
 ];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_video'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_video'],
+$GLOBALS['TL_DCA']['tl_article']['fields']['articleVideo'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['articleVideo'],
     'inputType' => 'fileTree',
     'eval' => array('tl_class'  => 'w50', 'fieldType' => 'radio', 'filesOnly' => true, 'extensions' => 'mp4' ),
     'sql'       => "blob NULL"
 ];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_shift_vertical'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_shift_vertical'],
+$GLOBALS['TL_DCA']['tl_article']['fields']['size'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_article']['size'],
+    'exclude' => true,
+    'inputType' => 'imageSize',
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval' => [
+        'rgxp' => 'natural',
+        'includeBlankOption' => true,
+        'nospace' => true,
+        'helpwizard' => true,
+        'tl_class' => 'clr w50',
+    ],
+    'options_callback' => function () {
+        return System::getContainer()->get('contao.image.image_sizes')->getOptionsForUser(BackendUser::getInstance());
+    },
+    'sql' => "varchar(64) NOT NULL default ''"
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['verticalBgShift'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['verticalBgShift'],
+    'inputType' => 'text',
+    'eval'      => array('tl_class'=>'w50'),
+    'sql'       => "text NULL"
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['viewBgVideoOnMobile'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'], 
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w50'),
+    'sql'       => "char(1) NOT NULL default ''" 
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['viewBgImageOnMobile'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'], 
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w50'),
+    'sql'       => "char(1) NOT NULL default ''" 
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['noBgVideoLoop'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['noBgVideoLoop'], 
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w50'),
+    'sql'       => "char(1) NOT NULL default ''" 
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['bgParallax'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['bgParallax'], 
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w50'),
+    'sql'       => "char(1) NOT NULL default ''" 
+];
+$GLOBALS['TL_DCA']['tl_article']['fields']['BgCssFilter'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_article']['BgCssFilter'],
     'inputType' => 'text',
     'eval'      => array('tl_class'=>'clr'),
     'sql'       => "text NULL"
 ];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_view_video_on_mobile'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_view_video_on_mobile'], 
-    'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50'),
-    'sql'       => "char(1) NOT NULL default ''" 
-];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_video_loop'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_video_loop'], 
-    'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50'),
-    'sql'       => "char(1) NOT NULL default ''" 
-];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_view_img_on_mobile'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_view_img_on_mobile'], 
-    'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50'),
-    'sql'       => "char(1) NOT NULL default ''" 
-];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_parallax'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_parallax'], 
-    'inputType' => 'checkbox', 
-    'eval'      => array('tl_class' => 'w50'),
-    'sql'       => "char(1) NOT NULL default ''" 
-];
-$GLOBALS['TL_DCA']['tl_article']['fields']['cc_bg_css_filter'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_article']['cc_bg_css_filter'],
-    'inputType' => 'text',
-    'eval'      => array('tl_class'=>'clr'),
-    'sql'       => "text NULL"
-];
-
-
-
-PaletteManipulator::create()
-    ->addLegend('cc_legend_bg', 'title_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('articleImage',               'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_video',               'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_shift_vertical',      'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_view_img_on_mobile',  'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_view_video_on_mobile','cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_parallax',            'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_video_loop',          'cc_legend_bg', PaletteManipulator::POSITION_APPEND)
-    ->addField('cc_bg_css_filter',              'cc_legend_bg', PaletteManipulator::POSITION_APPEND)    
-
-    ->applyToPalette('default', 'tl_article') 
-;
--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,11 +1,12 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_article']['cc_legend_bg']                   = 'Hintergrundbild oder -video';
-$GLOBALS['TL_LANG']['tl_article']['articleImage']                    = array ( 'Artikelhintergrundbild', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_video']                    = array ( 'Artikelhintergrundvideo', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_video_loop']               = array ( 'Video in Endlosschleife abspielen', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_view_video_on_mobile']     = array ( 'Artikelhintergrundvideo in der mobilen Ansicht ausblenden', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_view_img_on_mobile']       = array ( 'Artikelhintergrundbild in der mobilen Ansicht ausblenden', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_shift_vertical']           = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_parallax']                 = array ( 'Parallax-Effekt', '' );
-$GLOBALS['TL_LANG']['tl_article']['cc_bg_css_filter']                 = array ( 'CSS-Filter', 'Bspw. blur(2px) grayscale(1) opacity(.5) usw. siehe <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">Link</a>' );
+$GLOBALS['TL_LANG']['tl_article']['articleBg'] = 'Hintergrundbild und/oder -video';
+$GLOBALS['TL_LANG']['tl_article']['articleImage'] = array ( 'Artikelhintergrundbild', '' );
+$GLOBALS['TL_LANG']['tl_article']['size'] = array ( 'Artikelhintergrundbildgröße', 'Aktuell werden nur vordefinierte Bildgrößen unterstützt!' );
+$GLOBALS['TL_LANG']['tl_article']['articleVideo'] = array ( 'Artikelhintergrundvideo', '' );
+$GLOBALS['TL_LANG']['tl_article']['noBgVideoLoop'] = array ( 'Video nicht in Endlosschleife abspielen', '' );
+$GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'] = array ( 'Artikelhintergrundvideo in der mobilen Ansicht ausblenden', '' );
+$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Artikelhintergrundbild in der mobilen Ansicht ausblenden', '' );
+$GLOBALS['TL_LANG']['tl_article']['verticalBgShift'] = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
+$GLOBALS['TL_LANG']['tl_article']['bgParallax'] = array ( 'Parallax-Effekt', '' );
+$GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. auf <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
