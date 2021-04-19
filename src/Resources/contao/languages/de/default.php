802ccd64eeb3012b28028fd862130e200501ec3a
Niels <info@heimseiten.de>
2021-04-19 12:01:10 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -5,8 +5,8 @@ $GLOBALS['TL_LANG']['tl_article']['articleImage'] = array ( 'Artikelhintergrundb
 $GLOBALS['TL_LANG']['tl_article']['size'] = array ( 'Artikelhintergrundbildgröße', 'Aktuell werden nur vordefinierte Bildgrößen unterstützt!' );
 $GLOBALS['TL_LANG']['tl_article']['articleVideo'] = array ( 'Artikelhintergrundvideo', '' );
 $GLOBALS['TL_LANG']['tl_article']['noBgVideoLoop'] = array ( 'Video nicht in Endlosschleife abspielen', '' );
-$GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'] = array ( 'Artikelhintergrundvideo in der mobilen Ansicht ausblenden', '' );
-$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Artikelhintergrundbild in der mobilen Ansicht ausblenden', '' );
+$GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'] = array ( 'Video in der mobilen Ansicht ausblenden', '' );
+$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Hintergrundbild in der mobilen Ansicht ausblenden', '' );
 $GLOBALS['TL_LANG']['tl_article']['verticalBgShift'] = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
 $GLOBALS['TL_LANG']['tl_article']['bgParallax'] = array ( 'Parallax-Effekt', '' );
 $GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. auf <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
