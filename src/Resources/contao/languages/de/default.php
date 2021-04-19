f4d80d771b04a3e3cc88ab3d7ea2d74a4450b5df
Niels <info@heimseiten.de>
2021-04-19 16:51:02 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -6,7 +6,7 @@ $GLOBALS['TL_LANG']['tl_article']['size'] = array ( 'Artikelhintergrundbildgr√∂√
 $GLOBALS['TL_LANG']['tl_article']['articleVideo'] = array ( 'Artikelhintergrundvideo', '' );
 $GLOBALS['TL_LANG']['tl_article']['noBgVideoLoop'] = array ( 'Video nicht in Endlosschleife abspielen', '' );
 $GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'] = array ( 'Video in der mobilen Ansicht ausblenden', '' );
-$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Hintergrundbild in der mobilen Ansicht ausblenden', '' );
+$GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Bild in der mobilen Ansicht ausblenden', '' );
 $GLOBALS['TL_LANG']['tl_article']['verticalBgShift'] = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
 $GLOBALS['TL_LANG']['tl_article']['bgParallax'] = array ( 'Parallax-Effekt', '' );
 $GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. auf <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
