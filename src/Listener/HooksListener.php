71f535d7b7020e28d8261847511fe7d6bd00e08f
Niels <info@heimseiten.de>
2021-04-21 18:04:47 +0200

    Update HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -14,24 +14,26 @@ class HooksListener
     public function onCompileArticle(FrontendTemplate $objTemplate, array $arrData, Module $module): void
     {
         if (TL_MODE == 'FE' && $objTemplate->type == 'article') {
-            $template = new FrontendTemplate('article_image');
-
-            $template->articleImage = $arrData['articleImage'];
-            $template->articleImageSize = $arrData['articleImageSize'];
-            $template->articleVideo = $arrData['articleVideo'];
-            $template->noBgVideoLoop = $arrData['noBgVideoLoop'];
-            $template->viewBgVideoOnMobile = $arrData['viewBgVideoOnMobile'];
-            $template->viewBgImageOnMobile = $arrData['viewBgImageOnMobile'];
-            $template->verticalBgShift = $arrData['verticalBgShift'];
-            $template->bgParallax = $arrData['bgParallax'];
-            $template->BgCssFilter = $arrData['BgCssFilter'];
-            $template->articleMinHeight = $arrData['articleMinHeight'];
-            $template->articleMaxWidth = $arrData['articleMaxWidth'];
-            
+            $mod_article_before_content_elements = new FrontendTemplate('mod_article_before_content_elements');
+            $mod_article_before_content_elements->articleImage = $arrData['articleImage'];
+            $mod_article_before_content_elements->articleImageSize = $arrData['articleImageSize'];
+            $mod_article_before_content_elements->articleVideo = $arrData['articleVideo'];
+            $mod_article_before_content_elements->noBgVideoLoop = $arrData['noBgVideoLoop'];
+            $mod_article_before_content_elements->viewBgVideoOnMobile = $arrData['viewBgVideoOnMobile'];
+            $mod_article_before_content_elements->viewBgImageOnMobile = $arrData['viewBgImageOnMobile'];
+            $mod_article_before_content_elements->verticalBgShift = $arrData['verticalBgShift'];
+            $mod_article_before_content_elements->bgParallax = $arrData['bgParallax'];
+            $mod_article_before_content_elements->BgCssFilter = $arrData['BgCssFilter'];
+            $mod_article_before_content_elements->articleMinHeight = $arrData['articleMinHeight'];
+            $mod_article_before_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
             $elements = $objTemplate->elements;
-            array_unshift($elements, $template->parse());
-            $objTemplate->elements = $elements;
+            array_unshift($elements, $mod_article_before_content_elements->parse());
 
+            $mod_article_after_content_elements = new FrontendTemplate('mod_article_after_content_elements');
+            $mod_article_after_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
+            array_push($elements, $mod_article_after_content_elements->parse());
+            
+            $objTemplate->elements = $elements;
             $objTemplate->style .= '--test: 34px;';
         }
     }
@@ -39,7 +41,6 @@ class HooksListener
     public function onParseTemplate(Template $objTemplate)
     {
         if (TL_MODE == 'FE' && $objTemplate->type == 'article') {
-            if ($objTemplate->articleMaxWidth) { $objTemplate->style .= 'max-width: ' . $objTemplate->articleMaxWidth . ';'; }
             if ($objTemplate->articleMinHeight) { $objTemplate->style .= 'min-height: ' . $objTemplate->articleMinHeight . ';'; }
         }
     }
