e532c68aa2b4d9a1fee84caaf60b3679496e546c
Niels <info@heimseiten.de>
2021-08-06 18:56:33 +0200

    Update HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -24,25 +24,10 @@ class HooksListener
             $mod_article_before_content_elements->verticalBgShift = $arrData['verticalBgShift'];
             $mod_article_before_content_elements->bgParallax = $arrData['bgParallax'];
             $mod_article_before_content_elements->BgCssFilter = $arrData['BgCssFilter'];
-            $mod_article_before_content_elements->articleMinHeight = $arrData['articleMinHeight'];
-            $mod_article_before_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
             $elements = $objTemplate->elements;
             array_unshift($elements, $mod_article_before_content_elements->parse());
-
-            $mod_article_after_content_elements = new FrontendTemplate('mod_article_after_content_elements');
-            $mod_article_after_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
-            array_push($elements, $mod_article_after_content_elements->parse());
             
             $objTemplate->elements = $elements;
-            $objTemplate->style .= '--test: 34px;';
-        }
-    }
-
-    public function onParseTemplate(Template $objTemplate)
-    {
-        if (TL_MODE == 'FE' && $objTemplate->type == 'article') {
-            if ($objTemplate->articleMinHeight) { $objTemplate->style .= 'min-height: ' . $objTemplate->articleMinHeight . ';'; }
-            if ($objTemplate->articleMaxWidth) { $objTemplate->class .= ' has_inside'; }
         }
     }
 
