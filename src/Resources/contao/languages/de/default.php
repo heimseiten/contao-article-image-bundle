818ed68ad43b00fc15d1654fe05196578bf369a1
Niels <info@heimseiten.de>
2021-08-06 18:57:48 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -10,7 +10,3 @@ $GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Bild in der 
 $GLOBALS['TL_LANG']['tl_article']['verticalBgShift'] = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
 $GLOBALS['TL_LANG']['tl_article']['bgParallax'] = array ( 'Parallax-Effekt', '' );
 $GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
-
-$GLOBALS['TL_LANG']['tl_article']['articleWidthHeight'] = 'Artikelbreite/-höhe';
-$GLOBALS['TL_LANG']['tl_article']['articleMaxWidth'] = array ( 'maximale Breite des Artikels', 'Bspw. 1024px oder 50vw' );
-$GLOBALS['TL_LANG']['tl_article']['articleMinHeight'] = array ( 'minimale Höhe des Artikels', 'Bspw. 30vw. vw bedeutet "Viewport Width", also Prozent der Breite des Bildschirmes. vh bedeutet "Viewport Height", also Prozent der Höhe des Bildschirmes' );
