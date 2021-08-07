02db27141c46ee8ccfb867fe56d96eccb5883999
Niels <info@heimseiten.de>
2021-08-07 15:19:51 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 if (TL_MODE == 'FE') {
+
     $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoarticleimage/contao_article_image_bundle.scss|static';
+
+    function getRgbaFromHexAndOpacity($hex, $opacity)
+    {
+        if ($opacity) {
+          $alpha = $opacity/100;
+        } else { $alpha = '1'; }
+        list($r, $g, $b) = sscanf($hex, "%02x%02x%02x");
+        return 'rgba('.$r.','.$g.','.$b.','.$alpha.')';
+    }
+
 }
