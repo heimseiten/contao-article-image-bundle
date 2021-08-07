8622e543b05ae2cfde08c7252dcd1e99559ad816
Niels <info@heimseiten.de>
2021-08-07 15:20:16 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,12 +1,14 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_article']['articleBgLegend'] = 'Hintergrundbild/-video';
-$GLOBALS['TL_LANG']['tl_article']['articleImage'] = array ( 'Artikelhintergrundbild', '' );
-$GLOBALS['TL_LANG']['tl_article']['articleImageSize'] = array ( 'Artikelhintergrundbildgröße', 'Aktuell werden nur vordefinierte Bildgrößen unterstützt!' );
-$GLOBALS['TL_LANG']['tl_article']['articleVideo'] = array ( 'Artikelhintergrundvideo', '' );
+$GLOBALS['TL_LANG']['tl_article']['articleBgLegend'] = 'Hintergrundbild/-video/-farbe';
+$GLOBALS['TL_LANG']['tl_article']['articleImage'] = array ( 'Bild', '' );
+$GLOBALS['TL_LANG']['tl_article']['articleImageSize'] = array ( 'Bildgröße', 'Aktuell werden nur vordefinierte Bildgrößen unterstützt!' );
+$GLOBALS['TL_LANG']['tl_article']['articleVideo'] = array ( 'Video', '' );
 $GLOBALS['TL_LANG']['tl_article']['noBgVideoLoop'] = array ( 'Video nicht in Endlosschleife abspielen', '' );
 $GLOBALS['TL_LANG']['tl_article']['viewBgVideoOnMobile'] = array ( 'Video in der mobilen Ansicht ausblenden', '' );
 $GLOBALS['TL_LANG']['tl_article']['viewBgImageOnMobile'] = array ( 'Bild in der mobilen Ansicht ausblenden', '' );
 $GLOBALS['TL_LANG']['tl_article']['verticalBgShift'] = array ( 'Hintergrund vertical nach oben verschieben in %', '' );
 $GLOBALS['TL_LANG']['tl_article']['bgParallax'] = array ( 'Parallax-Effekt', '' );
-$GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
+$GLOBALS['TL_LANG']['tl_article']['BgCssFilter'] = array ( 'CSS-Filter für Bild und Video', 'Bspw. opacity(.2) grayscale(1) usw. siehe Bsp. <a href="https://blog.kulturbanause.de/2015/03/css-filter-effekte/" target="_blank">kulturbanause.de</a>' );
+$GLOBALS['TL_LANG']['tl_article']['bgColor'] = array ( 'Farbe', 'Farbe (hexadezimal bspw. ff0000 für rot) und Deckkraft (in Prozent bspw. 75)' );
+$GLOBALS['TL_LANG']['tl_article']['fontColor'] = array ( 'Textfarbe', 'Farbe (hexadezimal bspw. ff0000 für rot) und Deckkraft (in Prozent bspw. 75)' );
