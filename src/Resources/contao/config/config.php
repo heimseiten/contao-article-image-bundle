33bb73dbeb1bea2f621315fa1e2aa0126533a4a2
Niels <info@heimseiten.de>
2022-08-20 18:33:55 +0200

    Unlocked Contao 5

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,16 +1,12 @@
 <?php
 
-if (TL_MODE == 'FE') {
+  $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoarticleimage/contao_article_image_bundle.scss|static';
 
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoarticleimage/contao_article_image_bundle.scss|static';
-
-    function getRgbaFromHexAndOpacity($hex, $opacity)
-    {
-        if ($opacity) {
-          $alpha = $opacity/100;
-        } else { $alpha = '1'; }
-        list($r, $g, $b) = sscanf($hex, "%02x%02x%02x");
-        return 'rgba('.$r.','.$g.','.$b.','.$alpha.')';
-    }
-
+  function getRgbaFromHexAndOpacity($hex, $opacity) {
+  if ($opacity) {
+    $alpha = $opacity/100;
+    } else { $alpha = '1'; }
+    list($r, $g, $b) = sscanf($hex, "%02x%02x%02x");
+    return 'rgba('.$r.','.$g.','.$b.','.$alpha.')';
+  }
 }
