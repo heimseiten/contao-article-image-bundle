b4cce2293f5772d1ad28ad4902e1e05c6b9fd79a
Niels <info@heimseiten.de>
2022-11-21 11:50:15 +0100

    Update jarallax_settings.js

--- src/Resources/public/jarallax_settings.js
+++ src/Resources/public/jarallax_settings.js
@@ -2,3 +2,11 @@ jarallax(document.querySelectorAll('.article_bg.jarallax'), {
     speed: 0.2,
     imgElement: '.jarallax picture',
 })
+
+setHeightForHeroImage()
+window.addEventListener('resize', setHeightForHeroImage)
+function setHeightForHeroImage() {
+    document.querySelectorAll('.jarallax.article_bg').forEach(e => {
+        e.setAttribute( 'style', '--element-height: ' + e.clientHeight + 'px;' )
+    })    
+}
