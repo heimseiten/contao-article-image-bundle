2e71e3f7de75b6f646fe8b13b88442dcb1695ed0
Niels <info@heimseiten.de>
2026-06-23 11:32:56 +0200

    Modernize bundle layout (AbstractBundle, resources at root)

--- src/HeimseitenContaoArticleImageBundle.php
+++ src/HeimseitenContaoArticleImageBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Heimseiten\ContaoArticleImageBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class HeimseitenContaoArticleImageBundle extends Bundle
+class HeimseitenContaoArticleImageBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
